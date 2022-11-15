01dcfc48798372a25b112a7b20b650f9088e4b7b
Petr Nedoma <nnapik@gmail.com>
2022-11-15 02:30:40 +0100

    changed Outsider to Navsteva, refactored handling of applications

--- cogs/prihlasky.py
+++ cogs/prihlasky.py
@@ -1,25 +1,32 @@
 import re
 import importlib
 from discord.ext import commands
+from discord import TextChannel
 
 class Prihlasky(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.init_role_name = 'Navsteva'
+        self.member_role_name = 'Member'
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        for role in member.guild.roles:
-            if (role.name == 'Outsider'):
-                await member.add_roles(role)
+        init_role = self.bot.get_role(init_role_name)
+        if (init_role is not None):
+            await member.add_roles(role)
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.name == 'prihlaska-prazdna':
-            nick = re.findall(r'Jméno postavy:?\s*(\w*)', message.content)
-            if nick is not None:
-                nickname = nick[0]
-                await message.channel.edit(name='prihlaska-' + nickname)
-                await message.channel.category.create_text_channel("prihlaska-prazdna")
+        if message.author.id == self.bot.user.id: #filter out own messages
+            return
+        if not hasattr(message.channel, 'name') or message.channel.name != 'prihlaska-prazdna':
+            return
+        nick = re.findall(r'Jméno postavy:?\s*(\w*)', message.content)
+        if nick is None:
+            return
+        nickname = nick[0]
+        await message.channel.edit(name='prihlaska-' + nickname)
+        await message.channel.category.create_text_channel("prihlaska-prazdna")
 
     @commands.command()
     async def archiv(self, ctx):
@@ -39,10 +46,18 @@ class Prihlasky(commands.Cog):
         channel = ctx.channel
         if channel.name.startswith('prihlaska-'):
             nick = re.match(r'prihlaska-(.*)', channel.name)[1].title()
-            messages = await channel.history(limit=123).flatten()
+            messages = [h async for h in channel.history(limit=123)]
             first = messages[-1]
             author = first.author
-            for role in channel.guild.roles:
-                if (role.name == 'Member'):
-                    await author.add_roles(role)
+            init_role = self.bot.get_role(ctx.guild, self.init_role_name)
+            member_role = self.bot.get_role(ctx.guild, self.member_role_name)
+            if (member_role is None or init_role is None):
+                raise GuildApplicationException("Couldn't identify roles")
+            await author.add_roles(member_role)
+            if ((author.get_role(init_role.id)) is not None):
+                await author.remove_roles(init_role, reason="invited")
             await author.edit(nick=nick)
+
+class GuildApplicationException(Exception):
+    def __init__(message):
+        super(message)
